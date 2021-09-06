173d8700af8d2860c83cac3666a4f2ca2157e3a7
Paulo Henrique <paulohrs@hotmail.com>
2021-09-06 13:59:57 -0300

    Subindo todos os arquivos

---
+++ backend/src/test/java/com/devesuperior/ds/vendas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devesuperior.ds.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
